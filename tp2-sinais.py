ad1afdc5de66e0cbfc67a8c6dc94d8087b6f85ef
Tarcisio <Tarcisio.Batista@unimontes.br>
2015-05-19 18:53:41 -0300

    o for nao esta dando certo, mudar abordagem

--- tp2-sinais.py
+++ tp2-sinais.py
@@ -9,32 +9,33 @@ def convolui(sinal1,sinal2):
       #rebatido.dom[-1] é o ultimo elemento do vetor
       ready = rebatido1.dslc(rebatido1.dom[-1]-sinal2.dom[0])
       ready.show()
-      sinal2.show()
-      Y = sinal(0,0)
       aux = 0
+      auxD = ready.dom[-1]
+      Y = sinal([],[])
       for s,t in zip(ready.dom,ready.img):
-            for j in sinal2.img:
+            for i,j in zip(sinal2.dom,sinal2.img):
                   if (s==i):
                         #aqui pegamos o ultimo elemento do dominio do vetor que esta sendo deslocado,
                         #ele será substituido algumas veses mas no final ficará sempre na posição correta
-                        if (Y.dom[0]==0):
-                              Y.dom[0] = ready.dom[-1]
-                              Y.img[0]= Y.img[0] + (t * j)
-                              ready.dslc(-1)
-                              aux = aux + 1
-                        else:
-                              Y.dom[aux] = Y.dom[aux-1]+1
-                              Y.img[aux]= Y.img[aux] + (t * j)
-                              ready.dslc(-1)
-                              aux++
+                        print 'xd={} xi={} hd={} hi={} aux={} auxD={}'. format(s,t,i,j,aux,auxD)
+                        Y.dom[aux] = auxD
+                        Y.img[aux]= Y.img[aux] + (t * j)
+                        ready.dslc(-1)
+                        aux += 1
+                        auxD += 1
+      return Y
       
 class sinal():
       def __init__(self, inicial, imagem):
+            if inicial == []:
+                  self.dom = np.zeros(shape=(1,100))
+                  self.img = np.zeros(shape=(1,100))
             #cria o vetor dominio do ponto inicial até o tamanho
             #do vetor de imagens
-            self.dom = array(range(inicial,imagem.size+inicial))
-            self.img = imagem            
-            
+            else:
+                  self.dom = array(range(inicial,imagem.size+inicial))
+                  self.img = imagem
+                       
       def rbt(self):
             #invertendo o vetor de imagens
             #invertendo apenas a ordem do elementos
@@ -59,10 +60,9 @@ def main():
       img2 = array([1,0,2])
       P = sinal(-1,img1)
       S = sinal(-3,img2)
-##      for i in range(len(P.dom)):
-##            print '{} {}'. format(i,P.dom[i])
-      for t,s in zip(P.dom,P.img):
-            print '{} {}'. format(t,s)
-      #convolui(P,S)     
-      #print np.convolve (img1,img2)
+      T = sinal([],[])
+      S.show()
+      P.show()
+      convolui(S,P)
+
 main()
